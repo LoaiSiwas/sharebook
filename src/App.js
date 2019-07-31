f30b7c65f89011efb521e0dd701cbe26c978ac8f
Loai Siwas <loai@hotmail.cl>
2019-07-31 11:37:21 +0300

    add AppBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import AppBar from './Components/Appbar';
 
 function App() {
   return (
     <div>
+      <AppBar></AppBar>
       <p>Components Here</p>
     </div>
   );
