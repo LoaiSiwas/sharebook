b3387d85b59054d4f3f9111a9ef2e185edd2c5c1
Loai Siwas <loai@hotmail.cl>
2019-07-31 11:57:25 +0300

    Add empty Body

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import AppBar from './Components/Appbar';
+import Body from './Components/Body'
 
 function App() {
   return (
     <div>
       <AppBar></AppBar>
+      <Body></Body>
       <p>Components Here</p>
     </div>
   );
