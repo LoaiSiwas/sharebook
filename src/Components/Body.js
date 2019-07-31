f14687df75f4523f6dc82f614828099abfd30f43
Loai Siwas <loai@hotmail.cl>
2019-07-31 12:04:43 +0300

    Add Grid

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-function Body() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function Body() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <p>Body Components Here</p>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={9}>
+          <Paper className={classes.paper}>Book List Here</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>User Inforamtion Here</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>Footer</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
-
-export default Body;
