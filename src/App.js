132b97533f21c2708fd3abe571fd3f3a125ddf82
Loai Siwas <loai@hotmail.cl>
2019-08-03 17:35:46 +0300

    Add comps

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import Grid from '@material-ui/core/Grid';
 import './App.css';
 import AppBar from './Components/Appbar';
 
+// Mahmoud import
+// import AppBar from './Components/Appbar';
+// import AppBar from './Components/Appbar';
+// import AppBar from './Components/Appbar';
+
+// Feras import
+// import AppBar from './Components/Appbar';
+// import AppBar from './Components/Appbar';
+// import AppBar from './Components/Appbar';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -33,7 +42,25 @@ function App() {
         <Paper className={classes.paper}>User Inforamtion Here<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></Paper>
       </Grid>
       <Grid item xs={12}>
-        <Paper className={classes.paper}>Any Extra Components Here</Paper>
+        <Paper className={classes.paper}>Mahmoud</Paper>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>Mahmoud</Paper>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>Mahmoud</Paper>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>Feras</Paper>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>Feras</Paper>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>Feras</Paper>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>Loai</Paper>
       </Grid>
       <Grid item xs={12}>
         <Paper className={classes.paper}>Footer</Paper>
