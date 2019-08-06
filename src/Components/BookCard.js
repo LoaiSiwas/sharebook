4d2785b741926b89f1bcfec318ec0bc4d033515f
fmb97 <jackxxii22@gmail.com>
2019-08-06 17:22:23 +0300

    couldn't work with material ui cards so making my own

--- src/Components/BookCard.js
+++ src/Components/BookCard.js
@@ -1,42 +1,42 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import firebase from 'firebase'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyATBG2Q0SG1Md7EePZjOG92M3YX8NpgBdA",
-    authDomain: "share-book-d99d5.firebaseapp.com",
-    databaseURL: "https://share-book-d99d5.firebaseio.com",
-    projectId: "share-book-d99d5",
-    storageBucket: "share-book-d99d5.appspot.com",
-    messagingSenderId: "938760144413",
-    appId: "1:938760144413:web:7d88e8975c088ee2"
+  apiKey: "AIzaSyAqTz8ekMXm9QxxrEPWmTL41baMANckhio",
+  authDomain: "books-d274e.firebaseapp.com",
+  databaseURL: "https://books-d274e.firebaseio.com",
+  projectId: "books-d274e",
+  storageBucket: "books-d274e.appspot.com",
+  messagingSenderId: "924584312516",
+  appId: "1:924584312516:web:f7818c5e00ad7260"
 };
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore()
 
+const bookList = document.querySelector('#book-grid')
+
 function renderBook(doc){
-    document.getElementById("title").innerHTML = doc.data().Title
-    document.getElementById("description").innerHTML = doc.data().Description
-    document.getElementById("image").src= doc.data().Image-URL
-    document.getElementById("image").alt= doc.data().Title
+  let div = document.createElement('div');
+  let img = document.createElement('img')
+  let title = document.createElement('h3');
+  let description = document.createElement('p');
+  
+  img.src = doc.data().ImageURL;
+  div.style.cssText = 'border-raduis:4px;border: 1px solid gray; width:400em;margin:5px;box-shadow: 0px 1px 3px 0px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 2px 1px -1px rgba(0,0,0,0.12);'
+  img.style.cssText = 'width:15em;'
+  description.style.cssText = 'margin:0 50px 0;'
+  
+  div.setAttribute('data-id', doc.id);
+  title.textContent = doc.data().Title;
+  description.textContent = doc.data().description;
+
+  div.appendChild(img);
+  div.appendChild(title);
+  div.appendChild(description);
+
+  document.getElementById("book-grid").appendChild(div);
 }
-
-
-const useStyles = makeStyles({
-  card: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
-});
 
 db.collection('books').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
@@ -45,29 +45,13 @@ db.collection('books').get().then((snapshot) => {
 })
 
 export default function MediaCard() {
-  const classes = useStyles();
-
+  const divStyle = {
+    margin: '40px',
+    display: 'flex',
+    flexwrap: 'wrap',
+  };
+  
   return (
-    <Card className={classes.card}>
-      <CardActionArea>
-      <img src="#" id="image" alt="Loading..."/>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2" id="title">
-            Loading...
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p" id="description">
-            Loading...
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          More details
-        </Button>
-      </CardActions>
-    </Card>
-  );
+    <div id="book-grid" style={divStyle}></div>
+  )
 }
