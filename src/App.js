b9a9e2b93800bb49e3403879311793fe46c7ff3c
Loai Siwas <loai@hotmail.cl>
2019-08-07 19:23:51 +0300

    Book Card is done, Book list almost done

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ import FilledInputAdornments from './Components/SignUpForm'
 // import AppBar from './Components/Appbar';
 
 // Feras import
-import BookCard from './Components/BookCard.js'
+import BooksList from './Components/BooksList';
+import BookCard from './Components/BookCard'
 // import AppBar from './Components/Appbar';
 // import AppBar from './Components/Appbar';
 
@@ -36,7 +37,7 @@ function App() {
         <Paper className={classes.paper}><AppBar></AppBar></Paper>
       </Grid>
       <Grid item xs={9}>
-        <Paper className={classes.paper}>Book List Here<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></Paper>
+        <Paper className={classes.paper}><BooksList /></Paper>
       </Grid>
       <Grid item xs={3}>
         <Paper className={classes.paper}>User Inforamtion Here<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></Paper>
@@ -51,7 +52,7 @@ function App() {
         <Paper className={classes.paper}>Mahmoud</Paper>
       </Grid>
       <Grid item xs={12}>
-        <Paper className={classes.paper}><BookCard /></Paper>
+        <Paper className={classes.paper}><BookCard></BookCard></Paper>
       </Grid>
       <Grid item xs={12}>
         <Paper className={classes.paper}>Feras</Paper>
--- src/Components/BooksList.js
+++ src/Components/BooksList.js
@@ -1,50 +1,54 @@
 import React from 'react';
 import firebase from 'firebase';
+import BookCard from './BookCard'
+import Grid from '@material-ui/core/Grid';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAqTz8ekMXm9QxxrEPWmTL41baMANckhio",
-    authDomain: "books-d274e.firebaseapp.com",
-    databaseURL: "https://books-d274e.firebaseio.com",
-    projectId: "books-d274e",
-    storageBucket: "books-d274e.appspot.com",
-    messagingSenderId: "924584312516",
-    appId: "1:924584312516:web:f7818c5e00ad7260"
+    apiKey: "AIzaSyATBG2Q0SG1Md7EePZjOG92M3YX8NpgBdA",
+    authDomain: "share-book-d99d5.firebaseapp.com",
+    databaseURL: "https://share-book-d99d5.firebaseio.com",
+    projectId: "share-book-d99d5",
+    storageBucket: "share-book-d99d5.appspot.com",
+    messagingSenderId: "938760144413",
+    appId: "1:938760144413:web:7d88e8975c088ee2"
   };
 
 firebase.initializeApp(firebaseConfig);
-  
-const db = firebase.firestore()
-  
-  function renderBook(doc){
-    let div = document.createElement('div');
-    let img = document.createElement('img')
-    let title = document.createElement('h2');
-    let Address = document.createElement('p');
-    let details = document.createElement('a')
+
+export default class BooksList extends React.Component {
+    state = {
+        books: null,
+    }
     
-    
-    img.src = doc.data().ImageURL;
-    details.style.cssText = 'padding: 0.5em 1em;margin-bottom:1em; display: inline-block;text-decoration: none;border: 1px solid silver;box-shadow: 0px 1px 3px 0px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 2px 1px -1px rgba(0,0,0,0.12);'
-    div.style.cssText = 'font-family: "Roboto", "Helvetica", "Arial", sans-serif;border-raduis:4px;border: 1px solid silver; width:15em;margin:5px;box-shadow: 0px 1px 3px 0px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 2px 1px -1px rgba(0,0,0,0.12);'
-    img.style.cssText = 'width:15em;'
-    Address.style.cssText = 'margin:0 3em 0; margin-bottom:0.5em;overflow:hidden,'
-  
-    div.setAttribute('data-id', doc.id);
-    details.setAttribute('href', 'showDetails(doc.id)');
-    title.textContent = doc.data().Title;
-    Address.textContent = 'Location: ' + doc.data().Address;
-    details.textContent = 'More details'
-  
-    div.appendChild(img);
-    div.appendChild(title);
-    div.appendChild(Address);
-    div.appendChild(details);
-  
-    document.getElementById("book-grid").appendChild(div);
-  }
-  
-  db.collection('books').get().then((snapshot) => {
-      snapshot.docs.forEach(doc => {
-          renderBook(doc);
-      })
-  })
+    constructor() {
+        super();
+        this.fetchBooksFromAPI();
+    }
+
+    fetchBooksFromAPI = () => {
+        const db = firebase.firestore()
+        db.collection('books').get().then((response) => {
+            this.setState({ books: response.docs })
+        })
+    }
+
+    render() {
+        if (this.state.books === null) {
+            return <h1>Loading ...</h1>
+        }
+        const bookCards =  this.state.books.map(book => (
+            <BookCard
+                title={book.data().Title}
+                img={book.data().img}
+                description={book.data().Description}
+                country={book.data().Country}
+             />
+        ))
+
+        return (
+            <Grid container>
+                {bookCards}
+            </Grid>
+        )
+    }
+}
