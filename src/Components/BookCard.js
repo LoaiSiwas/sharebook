9440f60d3ff552b64fd2f4f6b409c82b6efafa53
fmb97 <jackxxii22@gmail.com>
2019-08-07 17:29:38 +0300

    fixing details button

--- src/Components/BookCard.js
+++ src/Components/BookCard.js
@@ -14,8 +14,6 @@ firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore()
 
-const bookList = document.querySelector('#book-grid')
-
 function renderBook(doc){
   let div = document.createElement('div');
   let img = document.createElement('img')
@@ -25,13 +23,13 @@ function renderBook(doc){
   
   
   img.src = doc.data().ImageURL;
-  details.style.cssText = 'padding: 0.5em 1em; display: inline-block;text-decoration: none;border: 1px solid silver;box-shadow: 0px 1px 3px 0px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 2px 1px -1px rgba(0,0,0,0.12);'
+  details.style.cssText = 'padding: 0.5em 1em;margin-bottom:1em; display: inline-block;text-decoration: none;border: 1px solid silver;box-shadow: 0px 1px 3px 0px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 2px 1px -1px rgba(0,0,0,0.12);'
   div.style.cssText = 'font-family: "Roboto", "Helvetica", "Arial", sans-serif;border-raduis:4px;border: 1px solid silver; width:400em;margin:5px;box-shadow: 0px 1px 3px 0px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 2px 1px -1px rgba(0,0,0,0.12);'
   img.style.cssText = 'width:15em;'
   Address.style.cssText = 'margin:0 3em 0; margin-bottom:0.5em;overflow:hidden,'
 
   div.setAttribute('data-id', doc.id);
-  details.setAttribute('href', 'javascript:showSignature(xyz)');
+  details.setAttribute('href', 'showDetails(doc.id)');
   title.textContent = doc.data().Title;
   Address.textContent = 'Location: ' + doc.data().Address;
   details.textContent = 'More details'
