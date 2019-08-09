4ba519304d629473f8f3622a182a6d495b906abc
fmb97 <jackxxii22@gmail.com>
2019-08-09 21:23:35 +0300

    restored first material card

--- src/Components/BookCard.js
+++ src/Components/BookCard.js
@@ -2,33 +2,27 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Grid from '@material-ui/core/Grid';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles(theme => ({
   card: {
     maxWidth: 300,
   },
   media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
+    height: 140,
   },
   expand: {
-    transform: 'rotate(0deg)',
     marginLeft: 'auto',
     transition: theme.transitions.create('transform', {
       duration: theme.transitions.duration.shortest,
     }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
   },
 }));
 
@@ -41,30 +35,25 @@ export default function BookCard(props) {
   return (
     <Grid item>
     <Card className={classes.card}>
-      <CardHeader
-        title={props.title}
-      />
-      <CardMedia
-        className={classes.media}
-        image={props.img}
-        title={props.title}
-      />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-        {props.description}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={props.img}
+          title={props.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          {props.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button className={clsx(classes.expand, {[classes.expandOpen]: expanded, })} onClick={handleExpandClick} aria-expanded={expanded} aria-label="show more" size="small" color="primary">
+          More Details
+        </Button>
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>          
